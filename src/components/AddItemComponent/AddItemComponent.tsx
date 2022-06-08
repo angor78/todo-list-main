9e652abce7debdc60ce5742f1c7df958299e85c4
Angor78 <angor78@gmail.com>
2022-06-08 16:14:06 +0300

    fix AdditemComponent for Chakra UI

---
+++ src/components/AddItemComponent/AddItemComponent.tsx
@@ -0,0 +1,54 @@
+import React, {ChangeEvent, KeyboardEvent, useState} from "react";
+import {Button, Flex, Input} from "@chakra-ui/react";
+
+export type addItemComponentPropsType = {
+  addItem: (newTitle: string) => void
+
+}
+export const AddItemComponent = (props: addItemComponentPropsType) => {
+  let [title, setTitle] = useState("")
+  let [error, setError] = useState<string | null>(null)
+
+  const addItem = () => {
+    let newTitle = title.trim();
+    if (newTitle !== "") {
+      props.addItem(newTitle);
+      setTitle("");
+    } else {
+      setError("Title is required");
+    }
+  }
+
+  const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.currentTarget.value)
+  }
+
+  const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+    setError(null);
+    if (e.charCode === 13) {
+      addItem();
+    }
+  }
+
+  return (
+    <div>
+      <Flex alignItems={'center'}>
+        <Input size='sm'
+               value={title}
+               onChange={onChangeHandler}
+               onKeyPress={onKeyPressHandler}
+               placeholder={error ? error : 'Add some...'}
+        />
+        <Button
+          onClick={addItem}
+          alignItems={'center'}
+          variant={'solid'}
+          colorScheme={'teal'}
+          size={'sm'}
+          mr={4}
+          ml={4}
+        >Add</Button>
+      </Flex>
+    </div>
+  )
+}
