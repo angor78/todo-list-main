064a5ce0397838cca2be2dd763db40b16117b086
Angor78 <angor78@gmail.com>
2022-06-09 09:24:59 +0300

    merge todolist and task in one state replace useState on useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,122 +1,64 @@
-import React, {useState} from 'react';
+import React, {useReducer} from 'react';
 import './App.css';
-import {TaskType, Todolist} from './components/Todolist/Todolist';
+import {Todolist} from './components/Todolist/Todolist';
 import {v1} from 'uuid';
 import {Box, ChakraProvider} from "@chakra-ui/react"
 import HeaderWithAction from "./components/HeaderWithAction/HeaderWithAction";
+import {addTodolistAC, reducer} from "./reducers";
+
 
 export type FilterValuesType = "all" | "active" | "completed";
-type TodolistType = {
+export type TodolistType = {
   id: string
   title: string
   filter: FilterValuesType
 }
-
-type TasksStateType = {
+export type TaskType = {
+  id: string
+  title: string
+  isDone: boolean
+}
+export type AllTasksType = {
   [key: string]: Array<TaskType>
 }
-
+export type StateType = {
+  todolists: Array<TodolistType>
+  tasks: AllTasksType
+}
 
 function App() {
   let todolistId1 = v1();
   let todolistId2 = v1();
 
-  let [todolists, setTodolists] = useState<Array<TodolistType>>([
-    {id: todolistId1, title: "What to learn", filter: "all"},
-    {id: todolistId2, title: "What to buy", filter: "all"}
-  ])
-
-  let [tasks, setTasks] = useState<TasksStateType>({
-    [todolistId1]: [
-      {id: v1(), title: "HTML&CSS", isDone: true},
-      {id: v1(), title: "JS", isDone: true}
+  let [state, dispatch] = useReducer(reducer, {
+    todolists: [
+      {id: todolistId1, title: "What to learn", filter: "all"},
+      {id: todolistId2, title: "What to buy", filter: "all"}
     ],
-    [todolistId2]: [
-      {id: v1(), title: "Milk", isDone: true},
-      {id: v1(), title: "React Book", isDone: true}
-    ]
+    tasks: {
+      [todolistId1]: [
+        {id: v1(), title: "HTML&CSS", isDone: true},
+        {id: v1(), title: "JS", isDone: true}
+      ],
+      [todolistId2]: [
+        {id: v1(), title: "Milk", isDone: true},
+        {id: v1(), title: "React Book", isDone: true}
+      ]
+    }
+
   });
 
-
-  function removeTask(id: string, todolistId: string) {
-    //достанем нужный массив по todolistId:
-    let todolistTasks = tasks[todolistId];
-    // перезапишем в этом объекте массив для нужного тудулиста отфилтрованным массивом:
-    tasks[todolistId] = todolistTasks.filter(t => t.id !== id);
-    // засетаем в стейт копию объекта, чтобы React отреагировал перерисовкой
-    setTasks({...tasks});
-  }
-
-  function addTask(title: string, todolistId: string) {
-    let task = {id: v1(), title: title, isDone: false};
-    //достанем нужный массив по todolistId:
-    let todolistTasks = tasks[todolistId];
-    // перезапишем в этом объекте массив для нужного тудулиста копией, добавив в начало новую таску:
-    tasks[todolistId] = [task, ...todolistTasks];
-    // засетаем в стейт копию объекта, чтобы React отреагировал перерисовкой
-    setTasks({...tasks});
-  }
-
   function addTodolist(title: string) {
-    let todolist: TodolistType = {id: v1(), title: title, filter: "all"};
-    setTodolists([todolist, ...todolists]);
-    setTasks({...tasks, [todolist.id]: []})
-  }
-
-  function changeStatus(id: string, isDone: boolean, todolistId: string) {
-    //достанем нужный массив по todolistId:
-    let todolistTasks = tasks[todolistId];
-    // найдём нужную таску:
-    let task = todolistTasks.find(t => t.id === id);
-    //изменим таску, если она нашлась
-    if (task) {
-      task.isDone = isDone;
-      // засетаем в стейт копию объекта, чтобы React отреагировал перерисовкой
-      setTasks({...tasks});
-    }
-  }
-
-  function changeFilter(value: FilterValuesType, todolistId: string) {
-    let todolist = todolists.find(tl => tl.id === todolistId);
-    if (todolist) {
-      todolist.filter = value;
-      setTodolists([...todolists])
-    }
-  }
-
-  function onChangeTodolistTitle(newTitle: string, todolistId: string) {
-    let todolist = todolists.find(tl => tl.id === todolistId);
-    if (todolist) {
-      todolist.title = newTitle;
-      setTodolists([...todolists])
-    }
-  }
-
-  function removeTodolist(id: string) {
-    // засунем в стейт список тудулистов, id которых не равны тому, который нужно выкинуть
-    setTodolists(todolists.filter(tl => tl.id !== id));
-    // удалим таски для этого тудулиста из второго стейта, где мы храним отдельно таски
-    delete tasks[id]; // удаляем св-во из объекта... значением которого являлся массив тасок
-    // засетаем в стейт копию объекта, чтобы React отреагировал перерисовкой
-    setTasks({...tasks});
-  }
-
-  function onChangeTitleTask(id: string, newTitle: string, todolistId: string) {
-    let todolistTasks = tasks[todolistId];
-    let task = todolistTasks.find(t => t.id === id);
-    if (task) {
-      task.title = newTitle;
-      setTasks({...tasks});
-    }
+    dispatch(addTodolistAC(title))
   }
 
   return (
-    <ChakraProvider >
+    <ChakraProvider>
       <div className="App">
         <HeaderWithAction addTodolist={addTodolist}/>
-        <Box alignItems={'top'} display={"flex"} flexWrap={'wrap'}>
-          {todolists.map(tl => {
-            let allTodolistTasks = tasks[tl.id];
+        <Box alignItems={'top'} display={"flex"} flexWrap={'wrap'} justifyContent={'center'} minHeight={800}>
+          {state.todolists.map(tl => {
+            let allTodolistTasks = state.tasks[tl.id];
             let tasksForTodolist = allTodolistTasks;
 
             if (tl.filter === "active") {
@@ -135,14 +77,8 @@ function App() {
               id={tl.id}
               title={tl.title}
               tasks={tasksForTodolist}
-              removeTask={removeTask}
-              changeFilter={changeFilter}
-              addTask={addTask}
-              changeTaskStatus={changeStatus}
               filter={tl.filter}
-              removeTodolist={removeTodolist}
-              onChangeTitleTask={onChangeTitleTask}
-              onChangeTodolistTitle={onChangeTodolistTitle}
+              dispatch={dispatch}
             />
             </Box>
           })
@@ -151,7 +87,7 @@ function App() {
       </div>
     </ChakraProvider>
   )
-    ;
+
 }
 
 export default App;
