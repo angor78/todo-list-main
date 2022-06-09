a0fe13cb2de837e8833732ea5589be9f421a09bc
Angor78 <angor78@gmail.com>
2022-06-09 09:21:02 +0300

    add ActionCreators to Todolist Component

--- src/components/Todolist/Todolist.tsx
+++ src/components/Todolist/Todolist.tsx
@@ -1,50 +1,48 @@
 import React, {ChangeEvent} from 'react';
 import {AddItemComponent} from "../AddItemComponent/AddItemComponent";
 import {EditableSpan} from "../EditableSpan/EditableSpan";
-import {FilterValuesType} from "../../App";
+import {FilterValuesType, TaskType} from "../../App";
 import {Box, Button, Checkbox, Heading} from "@chakra-ui/react";
 import {DeleteIcon, EditIcon} from "@chakra-ui/icons";
+import {
+  ActionType,
+  addTaskAC,
+  changeFilterAC, changeTaskStatusAC,
+  changeTitleTaskAC,
+  changeTodolistTitleAC,
+  removeTaskAC, removeTodolistAC
+} from "../../reducers";
 
-export type TaskType = {
-  id: string
-  title: string
-  isDone: boolean
-}
+
 
 type PropsType = {
   id: string
   title: string
   tasks: Array<TaskType>
-  removeTask: (taskId: string, todolistId: string) => void
-  changeFilter: (value: FilterValuesType, todolistId: string) => void
-  addTask: (title: string, todolistId: string) => void
-  changeTaskStatus: (id: string, isDone: boolean, todolistId: string) => void
-  removeTodolist: (id: string) => void
   filter: FilterValuesType
-  onChangeTitleTask: (taskId: string, newTitle: string, todolistId: string) => void
-  onChangeTodolistTitle: (newTitle: string, todolistId: string) => void
+  dispatch: (action: ActionType) => void
 }
 
 export function Todolist(props: PropsType) {
 
 
-  const removeTodolist = () => props.removeTodolist(props.id)
+  const removeTodolist = () => props.dispatch(removeTodolistAC(props.id))
 
-  const onAllClickHandler = () => props.changeFilter("all", props.id);
-  const onActiveClickHandler = () => props.changeFilter("active", props.id);
-  const onCompletedClickHandler = () => props.changeFilter("completed", props.id);
+  const onAllClickHandler = () => props.dispatch(changeFilterAC("all", props.id));
+  const onActiveClickHandler = () => props.dispatch(changeFilterAC("active", props.id));
+  const onCompletedClickHandler = () => props.dispatch(changeFilterAC("completed", props.id));
 
   function addTask(newTitle: string) {
-    props.addTask(newTitle, props.id)
+    props.dispatch(addTaskAC(newTitle, props.id))
   }
 
   function onChangeTodolistTitle(newTitle: string) {
-    props.onChangeTodolistTitle(newTitle, props.id)
+    props.dispatch(changeTodolistTitleAC(newTitle, props.id))
   }
 
   return <div>
-    <Box  minW='200' >
-      <Box display={'inline-block'} color={'gray.500'}>
+    <Box  minW='200'  >
+      <Box display={'inline-block'} color={'gray.500'} >
         <Button onClick={onAllClickHandler}
                 backgroundColor={props.filter === 'all' ? "teal.100" : undefined}>All</Button>
         <Button onClick={onActiveClickHandler}
@@ -62,17 +60,17 @@ export function Todolist(props: PropsType) {
 
       <ul>
         {props.tasks.map(t => {
-          const onClickHandler = () => props.removeTask(t.id, props.id)
+          const onClickHandler = () => props.dispatch(removeTaskAC(t.id,props.id))
           const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
             let newIsDoneValue = e.currentTarget.checked;
-            props.changeTaskStatus(t.id, newIsDoneValue, props.id);
+            props.dispatch(changeTaskStatusAC(t.id, newIsDoneValue, props.id));
           }
 
           function onChangeTitleTask(newTitle: string) {
-            props.onChangeTitleTask(t.id, newTitle, props.id)
+            props.dispatch(changeTitleTaskAC(t.id, newTitle, props.id))
           }
 
-          return <Box p='3' mt='2' mb='2' fontSize='18px' color={'gray.500'}  bg={"blackAlpha.100"} borderRadius={5} listStyleType={'none'} textAlign={'right'}>
+          return <Box p='3' mt='2' mb='2' fontSize='18px' color={'gray.500'}  bgColor={'whiteAlpha.800'} borderRadius={5} listStyleType={'none'} textAlign={'right'}>
             <li key={t.id} className={t.isDone ? "is-done" : ""}>
               {/*<input type="checkbox" onChange={onChangeHandler} checked={t.isDone}/>*/}
               <Checkbox onChange={onChangeHandler} defaultChecked={t.isDone} colorScheme='teal' float={'left'} mr={'5'}/>
