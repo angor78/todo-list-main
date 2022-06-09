b7fa114a0f44bea66ed2f5d3a5c5da3a4912a9db
Angor78 <angor78@gmail.com>
2022-06-09 09:28:19 +0300

    add items to tasks

--- src/App.tsx
+++ src/App.tsx
@@ -33,16 +33,18 @@ function App() {
   let [state, dispatch] = useReducer(reducer, {
     todolists: [
       {id: todolistId1, title: "What to learn", filter: "all"},
-      {id: todolistId2, title: "What to buy", filter: "all"}
+      {id: todolistId2, title: "What to buy", filter: "all"},
     ],
     tasks: {
       [todolistId1]: [
         {id: v1(), title: "HTML&CSS", isDone: true},
-        {id: v1(), title: "JS", isDone: true}
+        {id: v1(), title: "JS", isDone: true},
+        {id: v1(), title: "React", isDone: true},
+        {id: v1(), title: "Redux", isDone: true},
       ],
       [todolistId2]: [
         {id: v1(), title: "Milk", isDone: true},
-        {id: v1(), title: "React Book", isDone: true}
+        {id: v1(), title: "React Book", isDone: true},
       ]
     }
 
