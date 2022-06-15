b619bebdd212519f5a33741170f1f72c332cd606
Angor78 <angor78@gmail.com>
2022-06-15 16:04:43 +0300

    add test with name'Should be remove todolist'

--- src/reducers.test.ts
+++ src/reducers.test.ts
@@ -6,7 +6,7 @@ import {
   changeTitleTaskAC,
   changeTodolistTitleAC,
   reducer,
-  removeTaskAC
+  removeTaskAC, removeTodolistAC
 } from "./reducers";
 import {StateType} from "./App";
 
@@ -163,4 +163,29 @@ test('Should be add todolist', () => {
   }
   let endState = reducer(startState, addTodolistAC('Active'))
   expect(endState.todolists.length).toBe(3)
+})
+test('Should be remove todolist', () => {
+  let todolistId1 = v1();
+  let todolistId2 = v1();
+
+  let startState: StateType = {
+    todolists: [
+      {id: todolistId1, title: "What to learn", filter: "all"},
+      {id: todolistId2, title: "What to buy", filter: "all"},
+    ],
+    tasks: {
+      [todolistId1]: [
+        {id: v1(), title: "HTML&CSS", isDone: true},
+        {id: v1(), title: "JS", isDone: true},
+        {id: v1(), title: "React", isDone: false},
+        {id: v1(), title: "Redux", isDone: false},
+      ],
+      [todolistId2]: [
+        {id: v1(), title: "Milk", isDone: true},
+        {id: v1(), title: "React Book", isDone: true},
+      ]
+    }
+  }
+  let endState = reducer(startState, removeTodolistAC(todolistId1))
+  expect(endState.todolists.length).toBe(1)
 })
