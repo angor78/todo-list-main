88fa98119c5454b65fd1173309b07c900ab0d61a
Angor78 <angor78@gmail.com>
2022-06-15 15:31:00 +0300

    add test with name'Should be add task'

---
+++ src/reducers.test.ts
@@ -0,0 +1,31 @@
+import {v1} from "uuid";
+import {addTaskAC, reducer} from "./reducers";
+import {StateType} from "./App";
+
+
+test('Should be add task', () => {
+  let todolistId1 = v1();
+  let todolistId2 = v1();
+
+  let startState: StateType = {
+    todolists: [
+      {id: todolistId1, title: "What to learn", filter: "all"},
+      {id: todolistId2, title: "What to buy", filter: "all"},
+    ],
+    tasks: {
+      [todolistId1]: [
+        {id: v1(), title: "HTML&CSS", isDone: true},
+        {id: v1(), title: "JS", isDone: true},
+        {id: v1(), title: "React", isDone: false},
+        {id: v1(), title: "Redux", isDone: false},
+      ],
+      [todolistId2]: [
+        {id: v1(), title: "Milk", isDone: true},
+        {id: v1(), title: "React Book", isDone: true},
+      ]
+    }
+  }
+  let endState = reducer(startState, addTaskAC('Title', todolistId1))
+  expect(endState.tasks[todolistId1][0].title).toBe("Title")
+  expect(endState.tasks[todolistId1].length).toBe(5)
+})
