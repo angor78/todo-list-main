cccf797ece77dce297bb6aebff367e7180ad786a
Angor78 <angor78@gmail.com>
2022-07-21 17:15:51 +0300

    add Display component

--- src/api/api.stories.tsx
+++ src/api/api.stories.tsx
@@ -61,6 +61,7 @@ export const GetTasks = (props: any) => {
     TodolistsAPI.getTasks(todolistId)
       .then(data => {
         setState(data.data)
+
       })
   }, [])
   return <div>{JSON.stringify(state)}</div>
@@ -68,76 +69,96 @@ export const GetTasks = (props: any) => {
 
 export const CreateTask = (props: any) => {
   const [state, setState] = useState<any>(null)
-  useEffect(() => {
-    const todolistId = 'f7d91822-39c3-4a5d-b50b-fd7f853a0297'
-    TodolistsAPI.createTask(todolistId, '2222')
+  const createTask= (todolistId:string, title:string) => {
+    TodolistsAPI.createTask(todolistId, title)
       .then(data => {
         setState(data.data)
       })
-  }, [])
-  return <div>{JSON.stringify(state)}</div>
+  }
+  return <DisplayTasks state={state}
+                  setState={setState}
+                  name={'createTask'}
+                  action={createTask}/>
 }
 
 export const DeleteTask = (props: any) => {
   const [state, setState] = useState<any>(null)
-  useEffect(() => {
-    const todolistId = 'f7d91822-39c3-4a5d-b50b-fd7f853a0297'
-    const taskId = '5c09dbc9-caaf-4ecb-b32c-622633f6d888'
+
+
+  const deleteTask = (todolistId: string, taskId: string) => {
     TodolistsAPI.deleteTask(todolistId, taskId)
       .then(data => {
         setState(data.data)
       })
-  }, [])
-  return <div>{JSON.stringify(state)}</div>
+  }
+  return <DisplayTasks state={state}
+                  setState={setState}
+                  name={'deleteTask'}
+                  action={deleteTask}/>
 }
+
 export const UpdateTask = (props: any) => {
   const [state, setState] = useState<any>(null)
-  const [todolistId, setTodolistId] = useState<any>('')
-  const [taskId, setTaskId] = useState<any>('')
-  const [title, setTitle] = useState<any>('')
-
-
-  const updateTask = () => {
+  const updateTask = (todolistId: string, taskId: string, title: string) => {
     TodolistsAPI.updateTask(todolistId, taskId, title)
       .then(data => {
         setState(data.data)
       })
   }
 
+  return <DisplayTasks state={state}
+                  setState={setState}
+                  name={'update task'}
+                  action={updateTask}/>
+}
 
-  return <div>
-    <Box border={'1px'} borderColor={'red'} borderRadius={'lg'} p={'2'} mb={'2'}>
-      <Badge colorScheme={'red'}>Response</Badge>
-      {JSON.stringify(state)}
-    </Box>
-    <Box  border={'1px'} borderColor={'teal'} borderRadius={'lg'} p={'2'}>
-      <Badge colorScheme={'teal'}>Tasks</Badge>
-      <GetTasks/>
-    </Box>
-    <Badge colorScheme={'teal'}>todolistId</Badge>
-    <Box >
-      <Input type="text"
-             value={todolistId}
-             placeholder={'todolistId'}
-             onChange={(e) => {
-               setTodolistId(e.currentTarget.value)
-             }}/>
-      <Badge colorScheme={'teal'}>taskId</Badge>
-      <Input type="text"
-             placeholder={'taskId'}
-             value={taskId}
-             onChange={(e) => {
-               setTaskId(e.currentTarget.value)
-             }}/>
-      <Badge colorScheme={'teal'}>title</Badge>
-      <Input type="text"
-             placeholder={'title'}
-             value={title}
-             onChange={e=>setTitle(e.currentTarget.value)}/>
-    </Box>
 
-    <Button onClick={updateTask} mt={'3'}>update task</Button>
-  </div>
+
+
+const DisplayTasks = (props: any) => {
+  const [todolistId, setTodolistId] = useState<any>('')
+  const [taskId, setTaskId] = useState<any>('')
+  const [title, setTitle] = useState<any>('')
+
+  const onClickHandler = () => {
+    props.action(todolistId, taskId, title)
+  }
+  return (
+    <Box>
+      <Box border={'1px'} borderColor={'red'} borderRadius={'lg'} p={'2'} mb={'2'}>
+        <Badge colorScheme={'red'}>Response</Badge>
+        {JSON.stringify(props.state)}
+      </Box>
+
+      <Box border={'1px'} borderColor={'teal'} borderRadius={'lg'} p={'2'}>
+        <Badge colorScheme={'teal'}>Tasks</Badge>
+        <GetTasks />
+      </Box>
+
+      <Badge colorScheme={'teal'}>todolistId</Badge>
+      <Box>
+        <Input type="text"
+               value={todolistId}
+               placeholder={'todolistId'}
+               onChange={(e) => {
+                 setTodolistId(e.currentTarget.value)
+               }}/>
+        <Badge colorScheme={'teal'}>taskId</Badge>
+        <Input type="text"
+               placeholder={'taskId'}
+               value={taskId}
+               onChange={(e) => {
+                 setTaskId(e.currentTarget.value)
+               }}/>
+        <Badge colorScheme={'teal'}>title</Badge>
+        <Input type="text"
+               placeholder={'title'}
+               value={title}
+               onChange={e => setTitle(e.currentTarget.value)}/>
+      </Box>
+      <Button onClick={onClickHandler} mt={'3'}>{props.name}</Button>
+    </Box>
+  )
 }
 
 
