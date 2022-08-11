cda90846a15ec0202e2d0f323a88117e7d78862f
Angor78 <angor78@gmail.com>
2022-08-11 19:36:43 +0300

    lesson 16 fix4 BrowserRouter

--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,13 @@ import reportWebVitals from './reportWebVitals';
 import AppWithRedux from "./AppWithRedux";
 import { store } from './state/store'
 import { Provider } from 'react-redux'
-import { HashRouter} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.render(
   <Provider store={store}>
-    <HashRouter>
+    <BrowserRouter>
       <AppWithRedux/>
-    </HashRouter>
+    </BrowserRouter>
   </Provider>, document.getElementById('root')
 )
 
